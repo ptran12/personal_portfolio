7c7120bef8720c91d120ad7516cd5a3054ecf40f
ptran12 <phitran.722@gmail.com>
2020-11-01 18:39:06 -0600

    organizing & cleaning

--- personal_portfolio/src/components/info.js
+++ personal_portfolio/src/components/info.js
@@ -6,23 +6,26 @@ export default function info() {
   return (
     <div className="container">
       <div className="my-5"></div>
-      <div className="display-3" style={{ color: "#FF8A00" }}>
-        <span style={{ color: "white" }}>Hello, I'm</span> <br />
+      <div className="display-3" style={{ color: "white" }}>
+        <span style={{ color: "white" }}>Hello, my name is</span> <br />
         Phi.
       </div>
-      <div className="h1 code mt-4 mb-3">{"{"}</div>
-      <div className="text-muted mx-5 my-4 h3 text-justify info">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et.
-        <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et.
+      <div style={{ color: "yellow" }} className="h1 code mt-4 mb-3">
+        {"{"}
       </div>
-      <div className="h1 code mt-2 mb-3">{"}"}</div>
+      <div className="mx-5 my-4 h3 text-justify info">
+        I really enjoy coding and learning new things.
+        <br /> I strive to always learn more and improve as much as possible so
+        that I can grow as a Developer and person.
+      </div>
+      <div style={{ color: "yellow" }} className="h1 code mt-2 mb-3">
+        {"}"}
+      </div>
       <div className="h1 mt-5">
-        <a className="mr-5 icon" href="">
+        <a className="mr-5 icon" href="https://github.com/ptran12">
           <Github />
         </a>
-        <a className="mr-5 icon" href="">
+        <a className="mr-5 icon" href="https://www.linkedin.com/in/phitran-/">
           <LinkedIn />
         </a>
       </div>
